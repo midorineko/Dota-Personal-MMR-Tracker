1f10302ebdbdb1e36f7b74aebe6c521fc6edc4bd
Steven Inouye <steven.inouye91@gmail.com>
2014-05-14 10:33:58 -0400

    Skills now adding to hero in DB

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -8,3 +8,17 @@ get '/hero_ability/:id' do
   @hero_for_abilities = Hero.find_by_id(params[:id])
   erb :hero_ability
 end
+
+get '/hero_ability/stats/:id/add' do
+  @hero_for_abilities = Hero.find_by_id(params[:id])
+  erb :stats_add
+end
+
+post '/hero_ability/stats/:id/add/commit' do
+  @add_hero_stats = Hero.find_by_id(params[:id])
+  @new_stats = Stat.create(params[:post])
+  @add_hero_stats.stats << @new_stats
+  @combined_hero = Hero.find_by_id(params[:id])
+  @combined_hero_stats = @combined_hero.stats.last
+  erb :added_stats
+end
